d8643ed59e166e9528857cb0ed6d06a03db3e71c
Mathoz <mathieu.lambertin03@gmail.com>
2021-04-05 16:46:02 +0200

    after one hour

--- src/Views/CustomizeImage.jsx
+++ src/Views/CustomizeImage.jsx
@@ -4,7 +4,10 @@ export class CustomizeImage extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Customize Image</h1>
+                <input type="text"/>
+                <input type="range" min="0" max="200" />
+                <p>...x ...px</p>
             </div>
         )
     }
--- src/Views/Temperature.jsx
+++ src/Views/Temperature.jsx
@@ -1,13 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "../Styles/Temperature.css";
 
 export class Temperature extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Welcome to temperature page</h1>
-            </div>
-        )
-    }
+  state = {
+    temperature: null,
+  };
+
+  handleTemperature = (evt) => {
+      this.setState({temperature: evt.target.value})
+  }
+
+  render() {
+    const {temperature} = this.state;
+    return (
+      <div id="temperature">
+        <h1>Temperature</h1>
+        <input
+          id="celsus"
+          type="number"
+          name="celsus"
+          value={temperature}
+          placeholder="Temperature in °C"
+          onChange={this.handleTemperature}
+        />
+        {this.state.temperature < 10 && <p id="cold">It's cold</p>}
+        {this.state.temperature >= 10 && this.state.temperature < 30 && <p id="nice">It's nice</p>}
+        {this.state.temperature >= 30 && <p id="warm">It's warm</p>}
+      </div>
+    );
+  }
 }
 
-export default Temperature
+export default Temperature;
